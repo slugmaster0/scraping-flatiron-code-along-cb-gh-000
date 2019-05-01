e6e308be3b7ad9c65e2a3007099d99a719dc6d92
Bryan Jimenez <bryanjim@csu.fullerton.edu>
2019-05-01 03:53:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,8 +7,6 @@ class Scraper
   
     def get_page
       Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      doc.css(".post").each do |post|
-      end
     end
     
     def get_courses
